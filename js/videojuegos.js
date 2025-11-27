e543c739b2793534cd1a31f367668603de37299c
mtoscano-dotco <mtoscano@unicesar.edu.co>
2025-11-27 12:44:53 -0500

    Add files via upload

---
+++ js/videojuegos.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("formGamer");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("¡Gracias por compartir tu opinión gamer! 🎮🔥");
+});
